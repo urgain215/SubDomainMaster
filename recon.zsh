ab836f499aecc8567ba335243c9d5a323f856622
urgain <urgain@kali>
2023-10-02 15:02:57 +0530

    [Urgain Kali] Root file changed and sub_jack add

--- recon.zsh
+++ recon.zsh
@@ -2,10 +2,12 @@
 
 target=$1
 
-mkdir -p "/root/SubDomainMaster/SUB_HUNT"
+mkdir -p "$( pwd )/SUB_HUNT"
 
 echo "\n ===> Recon Start For $target \n"
 
 ./sub_hunt.zsh $target
+
+./sub_jack.zsh $target
 
 echo "\n ===> Recon Complete For $target \n"
